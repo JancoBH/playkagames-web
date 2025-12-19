b97be15672002c19601ccac963d1611c776a3b5e
JancoBH <jancoboscan@gmail.com>
2025-12-18 23:54:27 -0300

    contact

--- src/app/features/logika/routes.ts
+++ src/app/features/logika/routes.ts
@@ -2,9 +2,11 @@ import {Route} from "@angular/router";
 import {Logika} from './logika';
 import {Privacy} from './pages/privacy/privacy';
 import {Terms} from './pages/terms/terms';
+import {Contact} from './pages/contact/contact';
 
 export const LOGIKA_ROUTES: Route[] = [
   {path: '', component: Logika},
   {path: 'privacy', component: Privacy},
   {path: 'terms', component: Terms},
+  {path: 'contact', component: Contact},
 ];
